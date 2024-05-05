9eb122b9579aaccb1c3addc5d211d70eb215a063
Dalton Walker <dev@daltonwalker.com>
2024-05-04 21:51:05 -0400

    this should work

--- frontend/src/routes/docs/+layout.server.ts
+++ frontend/src/routes/docs/+layout.server.ts
@@ -5,7 +5,8 @@ import path from 'path';
 
 export const load: LayoutServerLoad = async () => {
 	if (DOCS_PATH) {
-		const repo_path = path.resolve('../../..');
+		const repo_path = path.resolve('..');
+		console.log(repo_path);
 		const fileNames = dirTree(repo_path + DOCS_PATH, { exclude: /.git/ });
 		return {
 			tree: JSON.stringify(fileNames, null, 2)
