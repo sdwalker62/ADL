1e3b7b0f542c9bf151426f02666cd7bfaa966268
Dalton Walker <dev@daltonwalker.com>
2024-05-05 18:22:15 -0400

    asd

--- frontend/ecosystem.config.cjs
+++ frontend/ecosystem.config.cjs
@@ -20,7 +20,8 @@ module.exports = {
 			repo: process.env.GIT_REPOSITORY,
 			path: process.env.DEPLOY_PATH,
 			'pre-deploy-local': '',
-			'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.cjs',
+			'post-deploy':
+				'cd frontend && npm install && npm run build && pm2 reload ecosystem.config.cjs',
 			'pre-setup': ''
 		}
 	}
